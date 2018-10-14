4445ffaa3da820ed0d22a9eed7bf1506e77971eb
Thomas Maestas <thomas76milton@gmail.com>
2018-10-13 22:55:41 -0600

    dailytech + filter

---
+++ client/app/app.js
@@ -0,0 +1,124 @@
+(function() {
+
+    var app = angular.module('app', ['ngRoute', 'ngCookies', 'ngResource']);
+
+    app.provider('books', ['constants', function (constants) {
+
+        var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function (value) {
+            includeVersionInTitle = value;
+        };
+
+        this.$get = function () {
+
+            var appName = constants.APP_TITLE;
+            var version = constants.APP_VERSION;
+
+            if (includeVersionInTitle) {
+                appName += ' ' + version;
+            }
+
+            var appDesc = constants.APP_DESCRIPTION;
+
+            return {
+                appName: appName,
+                appDesc: appDesc
+            };
+        };
+
+    }]);
+
+    app.config(['booksProvider', '$routeProvider', '$logProvider', '$httpProvider', '$provide', function (booksProvider, $routeProvider, $logProvider, $httpProvider, $provide) {
+
+        $provide.decorator('$log', ['$delegate', 'books', logDecorator]);
+
+        booksProvider.setIncludeVersionInTitle(true);
+        $logProvider.debugEnabled(true);
+
+        $httpProvider.interceptors.push('bookLoggerInterceptor');
+
+        $routeProvider
+            .when('/', {
+                templateUrl: '/app/templates/read.html',
+                controller: 'BooksController',
+                controllerAs: 'books'
+            })
+            .when('/AddBook', {
+                templateUrl: '/app/templates/addBook.html',
+                controller: 'AddBookController',
+                controllerAs: 'bookAdder'
+            })
+            .when('/archives', {
+                controller:'ArchivesController',
+                templateUrl: '/app/templates/archives.html'
+            })  
+            .when('/EditBook/:bookID', {
+                templateUrl: '/app/templates/editBook.html',
+                controller: 'EditBookController',
+                controllerAs: 'bookEditor'
+            })
+            .otherwise('/');
+
+    }]);
+
+    app.run(['$rootScope', function($rootScope) {
+
+        $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+
+            //console.log('successfully changed routes');
+
+        });
+
+        $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+
+            console.log('error changing routes');
+
+            console.log(event);
+            console.log(current);
+            console.log(previous);
+            console.log(rejection);
+
+        });
+
+    }]);
+
+    function logDecorator($delegate, books) {
+
+        function log(message) {
+            message += ' - ' + new Date() + ' (' + books.appName + ')';
+            $delegate.log(message);
+        }
+
+        function info(message) {
+            $delegate.info(message);
+        }
+
+        function warn(message) {
+            $delegate.warn(message);
+        }
+
+        function error(message) {
+            $delegate.error(message);
+        }
+
+        function debug(message) {
+            $delegate.debug(message);
+        }
+
+        function awesome(message) {
+            message = 'Awesome!!! - ' + message;
+            $delegate.debug(message);
+        }
+
+        return {
+            log: log,
+            info: info,
+            warn: warn,
+            error: error,
+            debug: debug,
+            awesome: awesome
+        };
+
+    }
+
+}());
--- server.js
+++ server.js
@@ -1,119 +1,68 @@
-const express = require('express'); 
-const app = express();
-const path = require('path');
-//const expressValidator = require('express-validator');
-//const request = require('request'); 
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose'); 
-// const db = mongojs('bloggerapp', ['citations']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-About = require('./models/about');
-Archives = require('./models/archive');
-Cat3 =require('./models/cat3'); 
-Post = require('./models/post'); 
+var routes = require('./server/routes/index');
+var readers = require('./server/routes/readers');
+var books = require('./server/routes/books');
 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '/server/views'));
+app.set('view engine', 'jade');
+
+app.use(favicon(path.join(__dirname, 'client/favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
- 
-app.use(express.static(path.join(__dirname + '/client')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client')));
 
-// Global vars
-app.use(function(req, res, next){
-	res.locals.errors = null;
-	next();
-});
+app.use('/', routes);
+app.use('/api/readers', readers);
+app.use('/api/books', books);
 
- 
-mongoose.connect('mongodb://localhost/poststore');
-var db = mongoose.connection;
-
-app.get('/', (req, res) => {
-	res.send('Please use /api/posts or /api/	');
-});
-  
- 
-app.get('/api/posts', (req, res) => {
-	Post.getPosts((err, posts) => {
-		if(err){
-			throw err;
-		}
-		res.json(posts);
-	});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/api/posts/:_id', (req, res) => {
-	Post.getPostById(req.params._id, (err, post) => {
-		if(err){
-			throw err;
-		}
-		res.json(post);
-	});
-});
-app.get('/api/posts/:_id', (req, res) => {
-	const posters = posts.find(c = c._id === parseInt(req.parems._id));
-	if (!posters) res.status(404).send('The post with given ID was ' + res.send(posters));
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.post('/api/posts', (req, res) => {
-	var post = req.body;
-	Post.addPost(post, (err, post) => {
-		if(err){
-			throw err;
-		}
-		res.json(post);
-	});
-});
+var debug = require('debug')('server');
 
-app.put('/api/posts/:_id', (req, res) => {
-	var id = req.params._id;
-	var post = req.body;
-	Post.updatePost(id, post, {}, (err, post) => {
-		if(err){
-			throw err;
-		}
-		res.json(post);
-	});
-});
+app.set('port', process.env.PORT || 3000);
 
-app.delete('/api/posts/:_id', (req, res) => {
-	var id = req.params._id;
-	Post.removePost(id, (err, post) => {
-		if(err){
-			throw err;
-		}
-		res.json(post);
-	});
-});
- /*
-app.get('/api/about', (req, res) => {
-	About.getAbouts((err, about) => {
-		if(err){
-			throw err;
-		}
-		res.json(about);
-	});
-});
+app.listen(app.get('port'));
 
-app.get('/api/archives', (req, res) => {
-	Archives.getArchives((err, archives) => {
-		if(err){
-			throw err;
-		}
-		res.json(archives);
-	});
-});
- 
-app.get('/api/cat3s', (req, res) => {
-	Cat3.getCat3s((err, cat3s) => {
-		if(err){
-			throw err;
-		}
-		res.json(cat3s);
-	});
-});
- */
-const port = process.env.PORT || 3010;
-app.listen(port, () => console.log(`Listening on port 3010`)); 
-console.log(`Running on port ${port}`);
+console.log('Listening on port: ' + app.get('port'));
 
- 
+module.exports = app;
