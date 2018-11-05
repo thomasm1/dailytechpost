335447039fac4af526029aa5f607d8378fdaf2f2
Thomas Maestas <thomas76milton@gmail.com>
2018-11-04 20:16:00 -0700

    everyday can be a great day cats

--- client/dist/bundle_daily_november.js
+++ client/dist/bundle_daily_november.js
@@ -82,7 +82,7 @@ __webpack_require__(9);
 __webpack_require__(10);
 __webpack_require__(11);
 __webpack_require__(12);
- //require('./js/marsreader');      
+ __webpack_require__(13);      
    
 
 
@@ -6378,6 +6378,42 @@ console.log('blogger-feb');
 };
 
 bloggerFeb();
+
+/***/ }),
+/* 13 */
+/***/ (function(module, exports) {
+
+ 
+
+
+var o = 'NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo';
+var url = "https://api.nasa.gov/planetary/apod?api_key=" + o;
+
+$.ajax({
+  url: url,
+  success: function(result){
+  if("copyright" in result) {
+    $("#copyright").text("Image Credits: " + result.copyright);
+  }
+  else {
+    $("#copyright").text("Image Credits: " + "Public Domain");
+  }
+  
+  if(result.media_type == "video") {
+    $("#apod_img_id").css("display", "block"); 
+    $("#apod_vid_id").attr("src", result.url);
+  }
+  else {
+    $("#apod_vid_id").css("display", "block"); 
+    $("#apod_img_id").attr("src", result.url);
+  }
+  $("#reqObject").text(url);
+  $("#returnObject").text(JSON.stringify(result, null, 4));  
+  $("#apod_explaination").text(result.explanation);
+  $("#apod_title").text(result.title);
+}
+});
+  
 
 /***/ })
 /******/ ]);
