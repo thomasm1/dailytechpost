16d3a11146d4be857ade1307b8f9968746cd678c
Thomas Maestas <thomasm1.maestas@gmail.com>
2019-03-26 18:09:04 -0600

    march catch-up

---
+++ client/public/src/js/blog-draft-march19.js
@@ -0,0 +1,200 @@
+var bloggerMar19 = function() {
+/*
+https://medium.com/s/teamhuman/the-right-kind-of-ai-with-kenric-mcdowell-7481b239a0cb
+
+https://medium.com/hummingbird-ventures/a-brief-history-of-blockchain-an-investors-perspective-387c440ad11c
+
+https://medium.com/@thinkoutsidetheblox/why-bitcoin-and-crypto-have-no-future-4f95980bb774
+
+https://medium.com/mit-technology-review/explainer-what-is-quantum-communication-527bb0f5d987
+
+https://media.consensys.net/18-predictions-for-2018-7a376ea7bd4b
+
+https://medium.com/s/welcome-to-blockchain/what-could-blockchain-do-for-healthcare-59c17245448e
+
+https://medium.com/mit-technology-review/russia-wants-to-cut-itself-off-from-the-global-internet-heres-what-that-really-means-c6f8851987ca
+
+https://hackernoon.com/blockchains-dont-scale-not-today-at-least-but-there-s-hope-2cb43946551a
+
+https://medium.com/s/story/algorithms-can-now-identify-cancerous-cells-better-than-humans-78e6518f65e8
+
+
+https://onezero.medium.com/computer-generated-faces-are-getting-real-b32e8902586d
+
+https://medium.com/s/story/the-best-strategies-to-boost-your-willpower-5eb1b2d9af2b
+
+
+*/
+
+    var blogpost3 = `
+	<p class="firstparagraph"> </p>
+	
+	<p class="quote"> Blockchains promise is clear. It could enable trust in a trustless world. A final solution to the ephemeral problem of the Byzantine general. Where intermediaries have historically been required to ensure trust, technology can first reduce our reliance before rendering them completely unnecessary. That means Airbnb without the need for the platform to take a cut of your profits. It means Uber without the faceless shark that everyone seems to hate.
+<br />
+	Blockchain’s killer app hasn’t been invented yet.
+	
+	</p>
+	
+ 
+	<p>1.
+	<a href="http://www.jareddiamond.org/Jared_Diamond/Guns,_Germs,_and_Steel.html" target="_blank">Guns, Germs, and Steel. &quot;Steel: The Fates of Human Societies.&quot; Diamond J (1997).</a>
+	</p>
+	`
+    var blogpost2 = `
+  <p class="firstparagraph">Security, security, another day another platform risk, or risks from your device, or risks from transactions, Today&#39;s
+  headline buried beneath a techy jargon provides a glimpse of the rising threat of web-application-based hackery:
+  Cryptojacking.
+  <i>Coindesk</i> reports, &quot; Opera browser introduces cryptocurrency miner protection for smartphones ... &quot;</p>
+  
+  <p class="quote">Cryptocurrency miners can overload smartphones&#39; CPUs, forcing 100 percent usage and potentially causing a phone
+  to overheat. And the damage can sometimes be permanent. According to a ZDNet article, one trojan generated so much
+  heat in a phone, its battery became swollen, permanently damaging the phone. While excessive ads were one reason
+  for the heat generation, the main cause was that the phone&#39;s CPU was hijacked to mine for Monero.
+  <sup>1</sup>
+  </p>
+  
+  <p>The larger issue here is that the lines between trusted mobile-app authorities, i.e., certified by Play Store or
+  Apple&#39;s is increasingly irrelevent. This is because an increasing number of app-users are ditching device-downloaded
+  apps, preferring for browser applications--hence, a much more appetizing market for the newest generation of blackhat
+  hackers, online thieves, peeping-toms, and other ill-wishers. There are a few driving factors for this issue--one
+  of which is the onset of Decentralized Applications (DAPPs), which use the
+  <i>browser</i> to interface between the Every-Day blockchain client/merchant/programmer and the actual byte-code of
+  the blockchain (via WEB).</p>
+  
+  <p>More importantly, the larger issue is that we live in an age in which the exclusive, native browser language since
+  1995, i.e. JavaScript. Security issues with browser-based apps were very low, because JavaScript never left the
+  browser, ever. Until 2009, Data, Servers were the domain of PHP, JAVA, Python, etc. or some other language for the
+  highly sensitive, dirty work of dipping into all that data sitting ontop of centralized servers around the world.
+  PHP wasn&#39;t easy, so power and responsibility usually followed with all the extra effort and expertise.</p>
+  
+  <p>2015 augured in the Node.js server-accessing (can create, read, update, and delete date) new-born capabilities of
+  JavaScript</p>
+  
+  
+  <p>1.
+  <a href="
+  https://www.coindesk.com/opera-browser-introduces-cryptocurrency-miner-protection-for-smartphones/
+  " target="_blank"> https://www.coindesk.com/opera-browser-introduces-cryptocurrency-miner-protection-for-smartphones/ </a>
+  </p>
+  `
+    var blogpost1 = `
+  <p class="firstparagraph">I couldn&#39;t help but revisit the
+  <strong>Environmental Sustainability</strong> problem I addressed in my February 28th post. This problem, experienced by
+  the top two crypto-currencies Bitcoin and Ethereum, is that the
+  <i>monetary</i> value of the currency is matched to the
+  <i>computing difficulty</i>--measured either by how much the computer is sweating, or by electricity KiloWatt Hours
+  per year.
+  <sup>1</sup> Digiconomist succinctly writes:</p>
+  
+  <p class="quote">The continuous block mining cycle incentivizes people all over the world to mine Bitcoin. As mining can provide a
+  solid stream of revenue, people are very willing to run power-hungry machines to get a piece of it. Over the years
+  this has caused the total energy consumption of the Bitcoin network to grow to epic proportions, as the price of
+  the currency reached new highs.
+  <sup>2</sup>
+  </p>
+  
+  <p>And, so the issue I&#39;m addressing is a side-effect of the deepening incentivization for miners to invest more
+  equipment, and in turn, more electricity. The article goes on to report Bitcoin&#39;s surpassing 50 TeraWatt-Hours-per-year
+  usage has surpassed that of Portugal and Singapore, which are 49.5 TWh per year, and 49.8 TWh per year, respectively.
+  In other words, the usage by the Bitcoin miner&#39;s cost (in electricity) compared with the miner&#39;s gain (in
+  Bitcoins accrued by incentivized mining) drives up and incentives a deeper Carbon Footprint.</p>
+  
+  
+  <p class="quote">Fueled by a meteoric rise in the Bitcoin price over the past few months, the power usage of the Bitcoin network has
+  been increasing at a feverish pace. Just a little over three months ago, at the start of November 2017, the Bitcoin
+  Energy Consumption Index was estimating the total electricity consumption of the Bitcoin network to be half of the
+  current amount. On the first day of November, the estimated consumption was equal to 24.3 TWh per year.
+  <sup>1</sup>
+  </p>
+  
+  <p>
+  Carbon Footprint, measured in this way, equates as a double-edged sword: The miner&#39;s electricity costs account for approximately
+  65% of the profit (Bitcoin units&#39; value, measured when mined). So the
+  <i>economic </i>usage means the cheapest available fuel is part of the business model--and it would lead to the cost
+  of the cheapest source of energy: Dirty Coal. At the end of the day, if the
+  <i>currency&#39;s </i>rising value means that more computationally-intensive mining is more and more profitable, then
+  perhaps the other edge looks preferred!
+  </p>
+  
+  <p>1.
+  <a href="
+  https://digiconomist.net/bitcoin-electricity-consumption-surpasses-singapore-portugal
+  " target="_blank"> https://digiconomist.net/bitcoin-electricity-consumption-surpasses-singapore-portugal </a>
+  </p>
+  
+  <p>2.
+  <a href="
+  https://digiconomist.net/bitcoin-energy-consumption
+  " target="_blank"> https://digiconomist.net/bitcoin-energy-consumption </a>
+  </p>
+  `
+    var url = [{
+            id: '3',
+            did: '19-03-03',
+            date: 'March 3-4, 2018 <br /> Weekend',
+            author: 'by Thomas Maestas, MA',
+            cat3: 'Sociology Tomorrow!',
+            title: 'A Return to the Question of Guns, Germs, and Steel',
+            post: blogpost3
+        },
+        {
+            id: '2',
+            did: '19-03-02',
+            date: 'March 2, 2018',
+            author: 'by Thomas Maestas, MA',
+            cat3: 'Web Dev Affairs',
+            title: 'Widening Circles of Mobile-App Security Risks',
+            post: blogpost2
+        },
+        {
+            id: '1',
+            did: '19-03-01',
+            date: 'March 1, 2018',
+            author: 'by Thomas Maestas, MA',
+            cat3: 'Musing Blockchain',
+            title: 'Blockchain Sustainability Issues and The Double-Edged Sword',
+            post: blogpost1
+        }
+    ];
+
+    for (i = 0; i < url.length; i++) {
+        var cat = ` 
+	  <div id="${url[i].did}" class="blogDiv"> 
+	  <hr />  
+	  <a href="#top"><button>Top</button></a>  
+	  <h4 class="title dailytech longtitle">OUR DAILY TECH:</h4>  
+	  <h5 id="cat3" class="  subdailytech">${url[i].cat3}</h5> 
+	  <p id="author" class="  author">${url[i].author}</p>   
+	  <h6  id="date" class="  chapternumber">${url[i].date}</h6>    
+	  <h6  id="title"   class="dailytitle chapternumber">${url[i].title}</h6>
+	  <br />
+	  <div id="post">${url[i].post}
+	  </div>
+	  </div>`
+
+        document.getElementById("paragraph-mas").innerHTML += cat;
+    }
+
+    var i;
+    for (i = 0; i < url.length; i++) {
+        var catMod = `
+	  <div id="${url[i].did}_mod" class="blogDivMod"> 
+	  <hr />  
+	  <a href="#top-mod"><button>Top</button></a>   
+	  <h4 class="title dailytech longtitle">OUR DAILY TECH:</h4>  
+	  <h5 id="cat3" class="  subdailytech">${url[i].cat3}</h5>  
+	  <p id="author" class="  author">${url[i].author}</p>   
+	  <h6  id="date" class="  chapternumber">${url[i].date}</h6>    
+	  <h6  id="title"   class="dailytitle chapternumber">${url[i].title}</h6>
+	  <br />
+	  <div id="post">${url[i].post}</div>
+	  </div>`;
+
+        document.getElementById("paragraph-ma-mod").innerHTML += catMod;
+    }
+
+    console.log('blogger-ma');
+    // console.log(angular.toJson(url));
+};
+
+bloggerMar19();
