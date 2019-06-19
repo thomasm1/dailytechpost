0f8c24d215354ce77e4694b0c735ff79817bf809
thomas maestas <thomasm1.maestas@gmail.com>
2019-06-18 19:36:39 -0600

    updated gulp and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
 const { src, dest, parallel } = require('gulp'); 
 const sass   = require('gulp-sass'); 
 const minifyCSS = require('gulp-csso');
+const imageMin = require('gulp-imagemin'); 
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
  
 function css() {
   return src('client/public/src/app.scss')
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(dest('client/public/dist'))
+    .pipe(browserSync.stream())
 }
  
+function img() {
+    return src('client/public/src/img/*')
+    .pipe(imageMin())
+    .pipe(dest('client/public/dist/img', { sourcemaps: true }))
+}
+   
 function js() {
   return src('client/public/src/js/*.js', { sourcemaps: true })
     .pipe(concat('app.min.js'))
-    .pipe(dest('dist', { sourcemaps: true }))
+    .pipe(dest('client/public/dist', { sourcemaps: true }))
 }
 
+exports.img = img;
 exports.js = js;
 exports.css = css; 
-exports.default = parallel(css, js);
+exports.default = parallel(img, css, js);
