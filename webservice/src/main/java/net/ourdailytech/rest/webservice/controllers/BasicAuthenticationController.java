dada0c3235e7dd65dccf5eec1f025294d31bfbda
thomas maestas <thomasm1.maestas@gmail.com>
2020-11-08 22:00:28 -0500

    webservice: authcontroller/dailyController

--- webservice/src/main/java/net/ourdailytech/rest/webservice/controllers/BasicAuthenticationController.java
+++ webservice/src/main/java/net/ourdailytech/rest/webservice/controllers/BasicAuthenticationController.java
@@ -7,17 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 import net.ourdailytech.rest.webservice.beans.AuthenticationBean;
 
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="*")
 @RestController
 public class BasicAuthenticationController {
 
 	@GetMapping(path="/api/dailytech/login")
+	@CrossOrigin(origins = "*")
 	public AuthenticationBean dailyTechEntityLogin() { 
 		 return new AuthenticationBean("You have successfully logged into /api/dailytech/login");
 	}
 	
-	@GetMapping(path="/dailytech/login")
-	public AuthenticationBean dailyTechLogin() { 
-		 return new AuthenticationBean("You have successfully logged into /dailytech/login");
-	}
+//	@GetMapping(path="/dailytech/login")
+//	@CrossOrigin(origins = "*")
+//	public AuthenticationBean dailyTechLogin() { 
+//		 return new AuthenticationBean("You have successfully logged into /dailytech/login");
+//	}
 }
--- webservice/src/main/java/net/ourdailytech/rest/webservice/controllers/DailyController.java
+++ webservice/src/main/java/net/ourdailytech/rest/webservice/controllers/DailyController.java
@@ -14,21 +14,25 @@ import net.ourdailytech.rest.webservice.beans.DailyTechBean;
 public class DailyController {
  
 	@RequestMapping(method=RequestMethod.GET, path = "/")
+	@CrossOrigin(origins = "*")
 	public String landing() {
 		return "Daily Tech Landing";
 	}
 	
 	@GetMapping(path="/dailytech")
+	@CrossOrigin(origins = "*")
 	public DailyTechBean dailyTechBean() { 
 		 return new DailyTechBean("Daily Tech Blog");
 	}
 	
 	@GetMapping(path="/dailytech/error")
+	@CrossOrigin(origins = "*")
 	public DailyTechBean errorPage() {
 		throw new RuntimeException("OOps! An error has occured"); 
 	}
 	
 	@GetMapping(path="/dailytech/user/{name}")
+	@CrossOrigin(origins = "*")
 	public DailyTechBean dailyTechUser(@PathVariable String name) { 
 		return new DailyTechBean(String.format("Daily Post Author: %s", name));
 	}
