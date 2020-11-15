0e33f469d52c54b94151145fe186274a1e3c14d1
thomas maestas <thomasm1.maestas@gmail.com>
2020-11-15 00:12:18 -0500

    add password encoder to JWT Auth

--- webservice/src/main/java/net/ourdailytech/rest/webservice/authJWT/JwtInMemoryUserDetailsService.java
+++ webservice/src/main/java/net/ourdailytech/rest/webservice/authJWT/JwtInMemoryUserDetailsService.java
@@ -15,8 +15,10 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
   static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
   static {
-    inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
-        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+    inMemoryUserList.add(new JwtUserDetails(1L, "admin",
+        "$2a$10$rPrOPenyZb3W0M074YR05Oxq1/tfh/BZtjh3cJMg8VUNMdxm/MIBu", "ROLE_USER_1"));
+    inMemoryUserList.add(new JwtUserDetails(2L, "in28minutes",
+            "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2")); 
   }
 
   @Override
