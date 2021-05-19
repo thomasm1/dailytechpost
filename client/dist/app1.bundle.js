f07f29b38fd7926e94a358a3f6c9a9df3667f9a3
thomas maestas <thomasm1.maestas@gmail.com>
2021-05-19 01:07:13 -0400

    may 18

--- client/dist/app1.bundle.js
+++ client/dist/app1.bundle.js
@@ -327,6 +327,18 @@ xhttp.send();
 var bloggerMay21 = function bloggerMay21() {
 
   // var blogpost2 =  ` 
+  {} /* <p class="firstparagraph">  Hence, the Application Interface Polygon's (MATIC) Layer-2 solutions.
+     </p> */
+  // <p class="quote"><i>  .<br /><br />
+  // </i><sup>1</sup> 
+  // </p>
+  // `;
+  // var blogcite2 = `
+  // <p class="footnotes">1. <a href="h "   target="_blank"> </a>
+  // </p>   `;
+
+
+  // var blogpost2 =  ` 
   // <p class="firstparagraph">, 
   // <p class="quote"><i>A Dogecoin (DOGE)-focused division of publicly traded over-the-counter software firm AppSwarm is looking to bring together global development teams to build off of the Dogecoin blockchain.<br /><br />
   // DogeLabs — a newly launched division of AppSwarm’s blockchain research lab, TulsaLabs — announced Wednesday a new initiative calling on DevOps teams to unite their efforts in building a “sort of decentralized network” of DOGE developers across the globe.</i><sup>1</sup> 
@@ -337,8 +349,11 @@ var bloggerMay21 = function bloggerMay21() {
   // </p>   `;
 
 
-  var blogpost1 = ' \n<p>Doubtless, New Chain Lifecycle is 5% utility, 5% hype and 90% Decentralized App developers\' community -- the ingredient <i>par excellence</i> for lasting hype and adoption. Here we have the exponential growth of Ethereum interest, let alone price of token. On the contrary we have Cardano Network, powered by Proof-of-Stake ADA token, which has yet to evolve past TestNet phase, a point of paralysis for Cardano chain DAPP development. One last step before final main net launch, and finally, we can witness David face Goliath...</p>\n\n<p class="quote"><i>This year, we are seeing the direct competitors clashing. Ethereum has made great strides to make it scalable and affordable, with the Berlin fork already finalized and London hard fork scheduled for July.\nIn the meantime, Cardano smart contracts should be entering the Cardano testnet by the end of this month.</i><sup>1</sup> \n</p>\n<p>Even if Cardono does launch main net, the multi-year delay in final completion of the the chain --for all its bells and whistles -- has stunted new development and developer solidarity. So why has ADA token reached its All-Time-High of $2.33, with a market cap of 66 billion</p>\n\n';
-  var blogcite1 = '\n \n<p class="footnotes">1. <a href="https://finance.yahoo.com/news/cardano-prepares-launch-alonzo-testnet-130000976.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAKH0bKJvZsFbQBykBexupKV-HvvhwSDQjmlBmyM4Q69AaNPYvc4Tk8LSJIyBqekdp0PAPL7kz0hTgJFEwOB3jchU4gzkApL6bQay9JZ2z16ddSW5RsCYEp049RRj-L_zFhrhze_83BrLCQjxz_-oOvLEbc8l3X_T4KdO8-eQf6cz"   target="_blank">cardano-prepares-launch-alonzo-testnet</a>\n</p>   \n';
+  var blogpost2 = ' \n<p class="firstparagraph">Ethereum\'s world virtual machine has proven utility already, and only the tip of the iceberg has been reached for potential. DeFi applications have locked 2/3 Ethereum, which boosts from $600 to $4000 summing to market cap of $388,000,000. <img src="dist/img/ada-btc-eth.png" class="zoom"   /> <p><i><small>Matic vs Ada vs ETH vs BTC performance (source: TradingView)</small></i></p> <p>The interesting matter is Ethereum\'s DeFi applicibility--from real estate to pools and loans--based on sophisticated smart contracts. Bitcoin, aside from form of value, has no parallel attraction...\n \n';
+  var blogcite2 = '\n<p class="footnotes">1. <a href="https://www.benzinga.com/node/21184597"   target="_blank">Is the Bitcoin-Ethereum \'Flippening\' a Likely Scenario?</a>\n</p>   ';
+
+  var blogpost1 = ' \n<p>Doubtless, New Chain Lifecycle is 5% utility, 5% hype and 90% Decentralized App developers\' community -- the ingredient <i>par excellence</i> for lasting hype and adoption. Here we have the exponential growth of Ethereum interest, let alone price of token. On the contrary we have Cardano Network, powered by Proof-of-Stake ADA token, which has yet to evolve past TestNet phase, a point of paralysis for Cardano chain DAPP development. One last step before final main net launch, and finally, we can witness David face Goliath...</p>\n\n<p class="quote"><i>This year, we are seeing the direct competitors clashing. Ethereum has made great strides to make it scalable and affordable, with the Berlin fork already finalized and London hard fork scheduled for July.\nIn the meantime, Cardano smart contracts should be entering the Cardano testnet by the end of this month.</i><sup>1</sup> \n</p>\n<p>Better late than never, the Cardano blockchain ecosystem boasts superior, more efficient, sophisticated smart contracting:</p>\n<p class="quote">Cardano (ADA) is a highly anticipated direct competitor to Ethereum, promising more scalability and smart contracts with the Alonzo upgrade.</p>\n<p>There are two obstinate problems:<i>Ethereum\'s monopolistic and universal DAPP evolution and ADA\'s stunted community development.</i></p>\n<p>First, All customized business applications, whether off-chain or on-, have built architectures depending on private/consortium blockchains that periodically sync with Ethereum mainnet. </p><p>Secondly, whether or not Cardono does launch main net, the multi-year delay in even <i>launching</i>chain mainnet --for all its bells and whistles -- has stunted new development and developer solidarity. Despite the author\'s praise for ADA, he concedes the infinitesimally small likelihood that Cardano developers will ever approach Ethereum network --</p> \n<p class="quote">Lastly, despite getting up to 7th rank, Cardano (ADA) is not likely to catch up with Ethereum\u2019s much larger dev pool and network effect.<sup>1</sup></p>\n<p>And so, beyond Ethereum 2\'s Proof-of-Stake transition on the near horizon, ethereum enjoys a burgeoning ecosystem of layer-2 sidechains, facilitated by the likes of those as Polygon (Matic) solutions. Even so, next to Polkadot ecosystem, only Cardano stands as a viable challenge, how ever miniscule it may be...</p>\n<p class="quote">\n*Ethereum holds (by far) the largest pool of developers, according to Electric Capital. As a number of open source dev ops tools are available to make remote work easier through collaboration, managing developers remains a serious cog in DeFi development. Yet Ethereum and its developer community have thus far been dominant in this sense.<br /><br />\n*In the last three years, Ethereum has widened its developer pool by 215%. Such a network effect would be exceedingly difficult to overcome.</i><sup>2</sup> \n</p></p>\n';
+  var blogcite1 = '\n \n<p class="footnotes">1. <a href="https://finance.yahoo.com/news/cardano-prepares-launch-alonzo-testnet-130000976.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAKH0bKJvZsFbQBykBexupKV-HvvhwSDQjmlBmyM4Q69AaNPYvc4Tk8LSJIyBqekdp0PAPL7kz0hTgJFEwOB3jchU4gzkApL6bQay9JZ2z16ddSW5RsCYEp049RRj-L_zFhrhze_83BrLCQjxz_-oOvLEbc8l3X_T4KdO8-eQf6cz"   target="_blank">cardano-prepares-launch-alonzo-testnet</a>\n</p>   \n<p class="footnotes">2. <a href="https://www.benzinga.com/node/21184597"   target="_blank">Is the Bitcoin-Ethereum \'Flippening\' a Likely Scenario?</a>\n</p>\n';
 
   var url = [
   // { 
@@ -350,14 +365,23 @@ var bloggerMay21 = function bloggerMay21() {
   //   title:'Doge Days are Here Again:  ',
   //   post:blogpost1,
   //  blogcite:blogcite1
-  //  },
+  //  },and the Law of Large Numbers
   {
+    id: '2',
+    did: 'may-21-05-18',
+    date: 'May 18, 2021',
+    author: 'by Thomas Maestas, MA',
+    cat3: 'Musing Blockchain',
+    title: 'ETH v. BTC v. ADA, Part I: The Law of Large Numbers & the Inefficiency of    ',
+    post: blogpost2,
+    blogcite: blogcite2
+  }, {
     id: '1',
     did: 'may-21-05-17',
     date: 'May 17, 2021',
     author: 'by Thomas Maestas, MA',
     cat3: 'Web Dev Affairs',
-    title: 'ETH v. BTC v. ADA: Varying levels of DAPP Community-Building and the Law of Large Numbers',
+    title: 'ETH v. BTC v. ADA, Part I: Varying levels of DAPP Community-Building ',
     post: blogpost1,
     blogcite: blogcite1
   }];
--- client/src/js/blog-draft-may21.js
+++ client/src/js/blog-draft-may21.js
@@ -3,7 +3,16 @@
 
 var bloggerMay21 = function () {
  
-  
+  // var blogpost2 =  ` 
+{/* <p class="firstparagraph">  Hence, the Application Interface Polygon's (MATIC) Layer-2 solutions.
+</p> */}
+// <p class="quote"><i>  .<br /><br />
+// </i><sup>1</sup> 
+// </p>
+// `;
+// var blogcite2 = `
+// <p class="footnotes">1. <a href="h "   target="_blank"> </a>
+// </p>   `;
 
 
 // var blogpost2 =  ` 
@@ -17,20 +26,40 @@ var bloggerMay21 = function () {
 // </p>   `;
  
 
+
+var blogpost2 =  ` 
+<p class="firstparagraph">Ethereum's world virtual machine has proven utility already, and only the tip of the iceberg has been reached for potential. DeFi applications have locked 2/3 Ethereum, which boosts from $600 to $4000 summing to market cap of $388,000,000. <img src="dist/img/ada-btc-eth.png" class="zoom"   /> <p><i><small>Matic vs Ada vs ETH vs BTC performance (source: TradingView)</small></i></p> <p>The interesting matter is Ethereum's DeFi applicibility--from real estate to pools and loans--based on sophisticated smart contracts. Bitcoin, aside from form of value, has no parallel attraction...
+ 
+`;
+var blogcite2 = `
+<p class="footnotes">1. <a href="https://www.benzinga.com/node/21184597"   target="_blank">Is the Bitcoin-Ethereum 'Flippening' a Likely Scenario?</a>
+</p>   `;
+ 
+
 var blogpost1 =  ` 
 <p>Doubtless, New Chain Lifecycle is 5% utility, 5% hype and 90% Decentralized App developers' community -- the ingredient <i>par excellence</i> for lasting hype and adoption. Here we have the exponential growth of Ethereum interest, let alone price of token. On the contrary we have Cardano Network, powered by Proof-of-Stake ADA token, which has yet to evolve past TestNet phase, a point of paralysis for Cardano chain DAPP development. One last step before final main net launch, and finally, we can witness David face Goliath...</p>
 
 <p class="quote"><i>This year, we are seeing the direct competitors clashing. Ethereum has made great strides to make it scalable and affordable, with the Berlin fork already finalized and London hard fork scheduled for July.
 In the meantime, Cardano smart contracts should be entering the Cardano testnet by the end of this month.</i><sup>1</sup> 
 </p>
-<p>Even if Cardono does launch main net, the multi-year delay in final completion of the the chain --for all its bells and whistles -- has stunted new development and developer solidarity. So why has ADA token reached its All-Time-High of $2.33, with a market cap of 66 billion</p>
-
+<p>Better late than never, the Cardano blockchain ecosystem boasts superior, more efficient, sophisticated smart contracting:</p>
+<p class="quote">Cardano (ADA) is a highly anticipated direct competitor to Ethereum, promising more scalability and smart contracts with the Alonzo upgrade.</p>
+<p>There are two obstinate problems:<i>Ethereum's monopolistic and universal DAPP evolution and ADA's stunted community development.</i></p>
+<p>First, All customized business applications, whether off-chain or on-, have built architectures depending on private/consortium blockchains that periodically sync with Ethereum mainnet. </p><p>Secondly, whether or not Cardono does launch main net, the multi-year delay in even <i>launching</i>chain mainnet --for all its bells and whistles -- has stunted new development and developer solidarity. Despite the author's praise for ADA, he concedes the infinitesimally small likelihood that Cardano developers will ever approach Ethereum network --</p> 
+<p class="quote">Lastly, despite getting up to 7th rank, Cardano (ADA) is not likely to catch up with Ethereum’s much larger dev pool and network effect.<sup>1</sup></p>
+<p>And so, beyond Ethereum 2's Proof-of-Stake transition on the near horizon, ethereum enjoys a burgeoning ecosystem of layer-2 sidechains, facilitated by the likes of those as Polygon (Matic) solutions. Even so, next to Polkadot ecosystem, only Cardano stands as a viable challenge, how ever miniscule it may be...</p>
+<p class="quote">
+*Ethereum holds (by far) the largest pool of developers, according to Electric Capital. As a number of open source dev ops tools are available to make remote work easier through collaboration, managing developers remains a serious cog in DeFi development. Yet Ethereum and its developer community have thus far been dominant in this sense.<br /><br />
+*In the last three years, Ethereum has widened its developer pool by 215%. Such a network effect would be exceedingly difficult to overcome.</i><sup>2</sup> 
+</p></p>
 `
 var blogcite1 = 
 `
  
 <p class="footnotes">1. <a href="https://finance.yahoo.com/news/cardano-prepares-launch-alonzo-testnet-130000976.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAKH0bKJvZsFbQBykBexupKV-HvvhwSDQjmlBmyM4Q69AaNPYvc4Tk8LSJIyBqekdp0PAPL7kz0hTgJFEwOB3jchU4gzkApL6bQay9JZ2z16ddSW5RsCYEp049RRj-L_zFhrhze_83BrLCQjxz_-oOvLEbc8l3X_T4KdO8-eQf6cz"   target="_blank">cardano-prepares-launch-alonzo-testnet</a>
 </p>   
+<p class="footnotes">2. <a href="https://www.benzinga.com/node/21184597"   target="_blank">Is the Bitcoin-Ethereum 'Flippening' a Likely Scenario?</a>
+</p>
 `; 
    
   var url =  
@@ -44,14 +73,24 @@ var blogcite1 =
     //   title:'Doge Days are Here Again:  ',
     //   post:blogpost1,
     //  blogcite:blogcite1
-    //  },
+    //  },and the Law of Large Numbers
+     { 
+      id:'2',
+      did:'may-21-05-18',
+      date:'May 18, 2021',
+      author:'by Thomas Maestas, MA',
+      cat3:'Musing Blockchain',
+      title:'ETH v. BTC v. ADA, Part I: The Law of Large Numbers & the Inefficiency of    ',
+      post:blogpost2,
+     blogcite:blogcite2
+     },
     { 
     id:'1',
     did:'may-21-05-17',
     date:'May 17, 2021',
     author:'by Thomas Maestas, MA',
     cat3:'Web Dev Affairs',
-    title:'ETH v. BTC v. ADA: Varying levels of DAPP Community-Building and the Law of Large Numbers',
+    title:'ETH v. BTC v. ADA, Part I: Varying levels of DAPP Community-Building ',
     post:blogpost1,
    blogcite:blogcite1
    }
