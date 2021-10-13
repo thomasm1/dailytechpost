2fb7eba3f7aa3d449224606aae37af29287febdb
Thomas Maestas <thomasm1.maestas@gmail.com>
2021-10-13 00:15:17 -0400

    Users & Posts test features

--- bitcoinBuyer11-ourDailyTech.net/KarateTests/src/test/java/api/DailyTechTest.java
+++ bitcoinBuyer11-ourDailyTech.net/KarateTests/src/test/java/api/DailyTechTest.java
@@ -3,6 +3,7 @@ package examples;
 import com.intuit.karate.junit5.Karate;
 
 class DailyTechTest {
+     
     
     // this will run all *.feature files that exist in sub-directories
     // see https://github.com/intuit/karate#naming-conventions   
