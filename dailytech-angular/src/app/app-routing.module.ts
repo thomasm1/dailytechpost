1f9180b458f24e6da69b3794dcbe761b2d585bea
Thomas Maestas <thomas1.maestas@gmail.com>
2026-01-05 20:21:03 -0500

    mid-migration part 2 of 4

--- dailytech-angular/src/app/app-routing.module.ts
+++ dailytech-angular/src/app/app-routing.module.ts
@@ -50,7 +50,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
     CommonModule
   ],
   exports: [
