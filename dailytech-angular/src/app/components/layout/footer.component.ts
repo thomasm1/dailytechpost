e206395cf1690dd6d019087b5138d0319db1f351
Thomas Maestas <thomas1.maestas@gmail.com>
2026-05-18 23:53:54 -0400

    ANGULAR: links usability

--- dailytech-angular/src/app/components/layout/footer.component.ts
+++ dailytech-angular/src/app/components/layout/footer.component.ts
@@ -25,11 +25,11 @@ import { Component, OnInit } from '@angular/core';
       bottom:0;
       width:100%;
       z-index:100; 
-      padding: 20px 0px 20px;
+      padding: 10px 0px 10px;
       color:rgb(220, 220, 247);
       text-align:center;
       line-height:2rem;
-    background-color: rgba(38, 23, 181, 0.7);
+      background-color: rgba(39, 23, 181, 0.5);
     }
     div.menu {
 
